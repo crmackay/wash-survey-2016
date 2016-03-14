6ecaac1ea0b96342d3e31f7fd9d36b83a2c6f26a
Chris <crmackay@gmail.com>
2016-03-14 19:34:08 -0400

    small updates to plan

--- csvToForm.py
+++ csvToForm.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python
 
+# Inputs: csv representation of a survey
+
+# Outputs:
+#   - form object
+#   - each individual question object
+#       - each has skip logic associated with it
+
+# a json representation of the form, questions, options, logic, etc.
+
 # open csv file, and read column labells
 
 # read each question
-
 
 
 # parser for each question type
---
+++ todos.md
@@ -0,0 +1,9 @@
+# TODO
+
+- make more generalizable
+
+- allow for several different surveys
+
+- individual login and roles
+
+- native application (notifactions, encryption etc.)
